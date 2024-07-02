f6cc08a1a7cd537ea9d7ebc46129c7b91042562b
nganp <nganphan.1545@gmail.com>
2024-07-02 19:20:17 +0700

    dfasd

--- src/main/java/com/hutech/webbanhang/controller/HomeController.java
+++ src/main/java/com/hutech/webbanhang/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "/home/index";
     }
